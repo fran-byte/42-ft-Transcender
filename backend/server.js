495b208b728057698153f426ae263f55243aefaf
allera-m <allera-m@DEBIAN12.myguest.virtualbox.org>
2026-02-04 00:38:45 +0100

    structure basic created

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: '¡El Backend funciona correctamente!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
